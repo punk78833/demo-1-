6a17a433f8fbca0f2bc98405e19c383a963a93d4
punk78833 <138745087+punk78833@users.noreply.github.com>
2025-09-15 16:14:09 +0530

    Create calculator.java

---
+++ calculator.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator Using Switch-Case");
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("Choose an operation: +, -, *, /");
+        char operation = scanner.next().charAt(0);
+
+        double result;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + result);
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Error! Division by zero.");
+                } else {
+                    result = num1 / num2;
+                    System.out.println("Result: " + result);
+                }
+                break;
+
+            default:
+                System.out.println("Invalid operation!");
+                break;
+        }
+
+        scanner.close();
+    }
+}
