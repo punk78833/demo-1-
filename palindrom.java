5af0d9be9842584c3014734108d27672dad8628b
punk78833 <138745087+punk78833@users.noreply.github.com>
2025-09-15 15:58:23 +0530

    Update and rename java palindrom to palindrom.java

--- palindrom.java
+++ palindrom.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class PalindromeNumberChecker {
+public class palindrom {
 
     public static boolean isPalindrome(int number) {
         int originalNumber = number;
